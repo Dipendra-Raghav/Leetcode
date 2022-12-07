c083bb224a5501aa3b8393eaac22805b23a25cf2
Dipendra Raghav <90553063+Dipendra-Raghav@users.noreply.github.com>
2022-12-07 13:02:12 +0530

    Time: 185 ms (85.39%), Space: 99.4 MB (14.91%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -10,13 +10,10 @@ public:
             left[i]=left[i-1]+nums[i];
         }
         right[n-1]=0;
+        int cnt=0;
         for(int i=n-2;i>=0;i--)
         {
             right[i]=right[i+1]+nums[i+1];
-        }
-        int cnt=0;
-        for(int i=0;i<n-1;i++)
-        {
             if(left[i]>=right[i])cnt++;
         }
         return cnt;
