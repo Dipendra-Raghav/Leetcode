e6d6ee1263448574554212cb2282bbe8bf2a51ca
Dipendra Raghav <90553063+Dipendra-Raghav@users.noreply.github.com>
2023-02-28 19:24:19 +0530

    Time: 27 ms (79.61%), Space: 39 MB (53.64%) - LeetHub

---
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    unordered_map<string,int> s;
+    vector<TreeNode*> v;
+    string fun(TreeNode* root)
+    {
+        string r1="",r2="";
+        if(root->right!=NULL)
+        {
+            r2=r2+fun(root->right);
+        } 
+        if(root->left!=NULL)
+        {
+            r1=r1+fun(root->left);
+        }   
+        string r=to_string(root->val)+","+r1+","+r2;
+        if(s.find(r)!=s.end())
+            {
+                if(s[r]==1)
+                {
+                   v.push_back(root);
+                } 
+                s[r]++;
+            }
+            else
+            {
+                s[r]=1;
+            }
+        return r;
+    }
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        string k=fun(root);
+        return v;
+    }
+};
