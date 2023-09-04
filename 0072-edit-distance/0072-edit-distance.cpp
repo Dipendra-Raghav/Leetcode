1497f94e4d33e7280d63b0253f50daa5ccc61d95
Dipendra Raghav <90553063+Dipendra-Raghav@users.noreply.github.com>
2023-09-04 21:28:01 +0530

    Time: 17 ms (41.62%), Space: 9.1 MB (41.83%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -40,7 +40,30 @@ public:
         
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size(),vector<int>(word2.size(),-1));
-        return solve(word1,word2,0,0,dp);
+        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,0));
+        // return solve(word1,word2,0,0,dp);
+       
+        for(int i=0;i<=word1.size();i++)
+        {
+            for(int j=0;j<=word2.size();j++)
+            {
+                  int a=INT_MAX;
+                  int b=INT_MAX;
+                  int c=INT_MAX;
+                  int d=INT_MAX;
+                
+                if (i == 0) {
+                    dp[i][j] = j;  
+                } else if (j == 0) {
+                    dp[i][j] = i;  
+                } else if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];  
+                } else {
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));  
+                }
+                
+            }
+        }
+        return dp[word1.size()][word2.size()];
     }
 };
