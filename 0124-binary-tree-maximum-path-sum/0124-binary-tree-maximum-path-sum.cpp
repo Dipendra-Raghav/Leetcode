b543daacc4d27a38a635b4b80592d80a179052ae
Dipendra Raghav <90553063+Dipendra-Raghav@users.noreply.github.com>
2023-10-04 13:00:19 +0530

    Time: 15 ms (87.80%), Space: 29 MB (5.12%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,45 +1,27 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public: 
-    int solve(TreeNode* root,int &k)
+public:
+    int mx=INT_MIN;
+    int solve(TreeNode* root)
     {
-        if(root==NULL)return 0;
-        int left=solve(root->left,k);
-        int right=solve(root->right,k);
-        
-        if(root->left==NULL && root->right==NULL)
-        {
-            int p=right+left+root->val;
-            k=k<p?p:k;
-           
-            return root->val;
-        }
-        
-        if(left>=right)
-        {
-            
-            int p=right+left+root->val;
-            k=k<p?p:k;
-            int ans= left+root->val>=root->val ?left+root->val : root-> val;
-            k=k<ans?ans:k;
-            return ans;
-            
-        }
-            int p=right+left+root->val;
-            k=k<p?p:k;
-       
-            int ans= right+root->val>=root->val ?right+root->val : root-> val;
-        k=k<ans?ans:k;
-        return ans; 
-        
-        
+        if(root==NULL)return -10000;
+        int a=solve(root->left);
+        int b=solve(root->right);
+        mx=max({mx,a+root->val,b+root->val,(a+b+root->val),root->val,a,b});
+        return max((max(a,b)+root->val),root->val);
     }
     int maxPathSum(TreeNode* root) {
-        int k=INT_MIN;
-       int ans= solve(root,k);
-        return k>=ans ? k : ans;
-        
-        
-        
+        solve(root);
+        return mx;
     }
 };
