8d7c3dd27a02ba2347b37a3039b3df9bb9b00d68
Dipendra Raghav <90553063+Dipendra-Raghav@users.noreply.github.com>
2024-01-25 09:30:55 +0530

    Time: 67 ms (18.98%), Space: 25.9 MB (5.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,51 +1,21 @@
 class Solution {
 public:
+    int solve(string &text1,string &text2,int i,int j,vector<vector<int>> &dp)
+    {
+        if(i>=text1.size() || j>=text2.size())return 0;
+        int incl=0,excl=0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        //include
+        if(text1[i]==text2[j]){
+            incl=1+solve(text1,text2,i+1,j+1,dp);
+        }
+        //exclude
+        excl=max(solve(text1,text2,i+1,j,dp),solve(text1,text2,i,j+1,dp));
+        return dp[i][j]=max(incl,excl);
+    }
     int longestCommonSubsequence(string text1, string text2) {
-        
-        int n=text1.size();
-        int m=text2.size();
-        vector<vector<int>> dp(n,vector<int>(m,0));
-        int chk=0;
-        int chkk=0;
-        
-        for(int i=0;i<m;i++)
-        {
-            if(chk==1)dp[0][i]=1;
-            else if(text1[0]==text2[i])
-            {
-                chk=1;
-                dp[0][i]=1;
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(chkk==1)dp[i][0]=1;
-            else if(text1[i]==text2[0])
-            {
-                chkk=1;
-                dp[i][0]=1;
-            }
-        }
-        
-        for(int i=1;i<n;i++)
-        {
-            for(int j=1;j<m;j++)
-            {
-                if(text1[i]==text2[j])
-                {
-                    
-                    dp[i][j]=1+dp[i-1][j-1];
-                    //cout<<i<<","<<j<<"->"<<dp[i][j]<<"  ";
-                }
-                else
-                {
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                    //cout<<i<<","<<j<<"->"<<dp[i][j]<<"  ";
-                }
-            }
-            //cout<<endl;
-        }
-        return dp[n-1][m-1];
+        vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),-1));
+        return solve(text1,text2,0,0,dp);
         
     }
 };
