13a9045828afd5a08797309225750dab321b7176
Dipendra Raghav <90553063+Dipendra-Raghav@users.noreply.github.com>
2024-02-23 05:17:43 +0530

    Time: 117 ms (89.28%), Space: 64.4 MB (62.95%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<pair<int,int>> graph(n+1,{0,0});
-        for(int i = 0; i < trust.size(); i++){
-            graph[trust[i][0]].first += 1;         
-            graph[trust[i][1]].second += 1;        
-        }
+        // vector<int> ans(10001,0);
+        // for(int i=0;i<trust.size();i++)
+        // {
+        //     ans[trust[i][0]]++;   
+        // }
+        // for(int i=0;i<trust.size();i++)
+        // {
+        //     if(ans[i]==0) return i;
+        // }
+        // return -1;
+        // // map<int, int> mp;
+        // // for(int i=0;i<n;i++)
+        // // {
+        // //     mp[trust[i][0]]++;
+        // // }
+        // // for(int i=0;i<=10000;i++)
+        // // {
+        // //     auto it=mp[i];
+        // //     if()
+        // // }
         
-        for(int i = 1; i <= n; i++){
-            if(graph[i].first == 0 && graph[i].second == n-1)   
-                return i;
+        vector<int> ans(n+1,0);
+        vector<int> ans1(n+1,0);
+        for(int i=0;i<trust.size();i++)
+        {
+            ans[trust[i][0]]++;
+            ans1[trust[i][1]]++;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(ans[i]==0 && ans1[i]==n-1)return i;
         }
         return -1;
+        
     }
 };
